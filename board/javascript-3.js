6d99b469d9cfdc87d087712cc0bb73594c21e209
UmaEra <124608386+NotYarazi@users.noreply.github.com>
2024-03-11 22:10:58 +0000

    [updated language board: javascript]

--- board/javascript-3.js
+++ board/javascript-3.js
@@ -6,5 +6,12 @@ function noname(){
     const cat4 = document.getelementbyid("cat4");
     const cat5 = document.getelementbyid("cat5");
     const cat6 = document.getelementbyid("cat6");
+    const cat7 = document.getelementbyid("cat7");
+    const cat8 = document.getelementbyid("cat8");
+    const cat9 = document.getelementbyid("cat9");
+    const cat10 = document.getelementbyid("cat10");
+    const cat11 = document.getelementbyid("cat11");
+    const cat12 = document.getelementbyid("cat12");
+    
       }
       
